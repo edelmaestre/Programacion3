ea99f765b57d5e36c155fbba8f8c19f67d40dbe0
Edelbero David Maestre Nuñez <39956223+edelmaestre@users.noreply.github.com>
2024-08-20 10:10:23 -0500

    Add files via upload

---
+++ clase1/clase1/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtdatos = New System.Windows.Forms.TextBox()
+        Me.guardar = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'txtdatos
+        '
+        Me.txtdatos.Location = New System.Drawing.Point(26, 35)
+        Me.txtdatos.Name = "txtdatos"
+        Me.txtdatos.Size = New System.Drawing.Size(100, 20)
+        Me.txtdatos.TabIndex = 1
+        '
+        'guardar
+        '
+        Me.guardar.Location = New System.Drawing.Point(184, 33)
+        Me.guardar.Name = "guardar"
+        Me.guardar.Size = New System.Drawing.Size(75, 23)
+        Me.guardar.TabIndex = 2
+        Me.guardar.Text = "Guardar"
+        Me.guardar.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(73, 90)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.guardar)
+        Me.Controls.Add(Me.txtdatos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtdatos As System.Windows.Forms.TextBox
+    Friend WithEvents guardar As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ clase1/clase1/Form1.vb
@@ -0,0 +1,27 @@
+﻿
+Imports System.IO
+Public Class Form1
+
+
+    Private ruta As String = Application.StartupPath & "\miarchivo.txt"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub guardar_Click(sender As Object, e As EventArgs) Handles guardar.Click
+        gurdar(txtdatos.Text)
+    End Sub
+
+    Private Sub gurdar(datos As String)
+        Dim sw As New StreamWriter(ruta, True)
+        sw.WriteLine(datos)
+
+        sw.Close()
+    End Sub
+
+   
+    Private Sub txtdatos_TextChanged(sender As Object, e As EventArgs) Handles txtdatos.TextChanged
+
+    End Sub
+End Class
